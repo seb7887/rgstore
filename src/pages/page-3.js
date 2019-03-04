e555e6f0706174fbcb101bcc4b8ba5a83675de29
Sebastian <segurasebastian7887@gmail.com>
2019-03-04 12:01:14 -0300

    GraphQL Queries

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -1,13 +1,53 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql, StaticQuery, Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+const getImageData = graphql`
+  {
+    allFile {
+      edges {
+        node {
+          relativePath
+          size
+          extension
+          birthTime
+        }
+      }
+    }
+  }
+`;
+
 const ThirdPage = () => (
   <Layout>
     <SEO title="Page Three" />
-    <h1>Page 3</h1>
+    <h1>Image File Data</h1>
+    <StaticQuery
+      query={getImageData}
+      render={data => (
+        <table>
+          <thead>
+            <tr>
+              <th>Relative Path</th>
+              <th>Size</th>
+              <th>Extension</th>
+              <th>Birth Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map((edge, index) => (
+              <tr key={index}>
+                <td>{edge.node.relativePath}</td>
+                <td>{edge.node.size}</td>
+                <td>{edge.node.extension}</td>
+                <td>{edge.node.birthTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    />
     <Link to="/">Go back to homepage</Link>
   </Layout>
 );
